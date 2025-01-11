94b4c4ab918c72a1a7cfaea4f0fcdb669be13378
tuesay <tuesayminecraft@gmail.com>
2025-01-11 20:12:06 +0800

    pep8

--- main_menu.py
+++ main_menu.py
@@ -10,8 +10,9 @@ from mark_shapes import *
 pygame.font.init()
 mixer.init()
 
-musics = ['interstellar.mp3','funky.mp3']
- 
+musics = ['interstellar.mp3', 'funky.mp3']
+
+
 def main(window):
     locked_pos = {}
     grid = create_grid(locked_pos)
@@ -24,71 +25,71 @@ def main(window):
     fall_speed = 0.27
     score = 0
     level_time = 0
-    
+
     while run:
         grid = create_grid(locked_pos)
         fall_time += clock.get_rawtime()
         clock.tick()
-        
-        if fall_time/1000>=fall_speed:
+
+        if fall_time / 1000 >= fall_speed:
             fall_time = 0
-            curr_piece.y+=1
-            if not valid_space(curr_piece,grid) and curr_piece.y > 0:
-                curr_piece.y-=1
-                change_piece=True
-        
-        
+            curr_piece.y += 1
+            if not valid_space(curr_piece, grid) and curr_piece.y > 0:
+                curr_piece.y -= 1
+                change_piece = True
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
-                    curr_piece.rotate+=1
-                    if not valid_space(curr_piece,grid):
-                        curr_piece.rotate-=1
+                    curr_piece.rotate += 1
+                    if not valid_space(curr_piece, grid):
+                        curr_piece.rotate -= 1
                 if event.key == pygame.K_DOWN:
-                    curr_piece.y+=1
-                    if not valid_space(curr_piece,grid):
-                        curr_piece.y-=1
+                    curr_piece.y += 1
+                    if not valid_space(curr_piece, grid):
+                        curr_piece.y -= 1
                 if event.key == pygame.K_LEFT:
-                    curr_piece.x-=1
-                    if not valid_space(curr_piece,grid):
-                        curr_piece.x+=1
+                    curr_piece.x -= 1
+                    if not valid_space(curr_piece, grid):
+                        curr_piece.x += 1
                 if event.key == pygame.K_RIGHT:
-                    curr_piece.x+=1
-                    if not valid_space(curr_piece,grid):
-                        curr_piece.x-=1
-                        
+                    curr_piece.x += 1
+                    if not valid_space(curr_piece, grid):
+                        curr_piece.x -= 1
+
         final_piece_pos = convert_shape_format(curr_piece)
-        for x,y in final_piece_pos:
-            if y>-1:
-                grid[y][x] = curr_piece.color         #change i j
-                
+        for x, y in final_piece_pos:
+            if y > -1:
+                grid[y][x] = curr_piece.color  # change i j
+
         if change_piece:
-            for x,y in final_piece_pos:
-                locked_pos[(x,y)] = curr_piece.color  #change i j
+            for x, y in final_piece_pos:
+                locked_pos[(x, y)] = curr_piece.color  # change i j
             curr_piece = next_piece
             next_piece = get_shape()
-            temp_score = clear_rows(grid,locked_pos)
-            score+=(temp_score*10)
+            temp_score = clear_rows(grid, locked_pos)
+            score += (temp_score * 10)
             change_piece = False
-        
-        draw_window(window,grid,score)
-        draw_next_shape(next_piece,window)
+
+        draw_window(window, grid, score)
+        draw_next_shape(next_piece, window)
         pygame.display.update()
-        
+
         if check_lost(locked_pos):
             run = False
-    
+
     pygame.display.quit()
+
 
 def main_menu(window):
     run = True
     while run:
         mixer.music.set_volume(0.1)
-        font_home = pygame.font.SysFont('comicsans', 40,True)
-        text = font_home.render("Press any key to play", 1, (180,60,255))
-        window.blit(text, [s_width/2 - 200, s_height/2 - 30])
+        font_home = pygame.font.SysFont('comicsans', 40, True)
+        text = font_home.render("Press any key to play", 1, (180, 60, 255))
+        window.blit(text, [s_width / 2 - 200, s_height / 2 - 30])
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -101,10 +102,6 @@ def main_menu(window):
                 main(window)
 
 
-window = pygame.display.set_mode((s_width,s_height))
+window = pygame.display.set_mode((s_width, s_height))
 pygame.display.set_caption("TETRIS")
-main_menu(window)               
-                
-    
-
-
+main_menu(window)
