f11312910ca2294b3cd48fdcd28a9fcc169f9502
tuesay <tuesayminecraft@gmail.com>
2025-01-31 19:53:18 +0800

    maked some serialization data tweaks

--- network/battle_server.py
+++ network/battle_server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import time
+import pickle
 
 
 class BattleServer:
@@ -18,30 +19,29 @@ class BattleServer:
             self.server_socket.bind((self.host, self.port))
             self.server_socket.listen(2)  # Ожидаем подключения двух клиентов
             print(f"Сервер запущен на {self.host}:{self.port}. Ожидание подключений...")
-            while True:
-                while len(self.clients) < 2:
+            while len(self.clients) < 2:
 
-                    client_socket, client_address = self.server_socket.accept()
-                    print(f"Подключен клиент: {client_address}")
-                    self.clients.append(client_socket)
+                client_socket, client_address = self.server_socket.accept()
+                print(f"Подключен клиент: {client_address}")
+                self.clients.append(client_socket)
 
-                    # Отправляем подтверждение подключения
+                # Отправляем подтверждение подключения
 
-                    client_socket.send("Подключено к серверу. Ожидаем второго игрока...".encode())
+                self.broadcast("Подключено к серверу. Ожидаем второго игрока...")
 
-                    # Если подключен только один клиент, отправляем ему сообщение о состоянии
-                    if len(self.clients) == 1:
-                        threading.Thread(target=self.notify_waiting, args=(client_socket,)).start()
+                # Если подключен только один клиент, отправляем ему сообщение о состоянии
+                if len(self.clients) == 1:
+                    threading.Thread(target=self.notify_waiting, args=(client_socket,)).start()
 
 
-                print("Оба игрока подключены. Начинаем игру!")
-                # Отправляем сообщение обоим клиентам о начале игры
-                time.sleep(1)
+            print("Оба игрока подключены. Начинаем игру!")
+            # Отправляем сообщение обоим клиентам о начале игры
+            time.sleep(1)
 
-                self.broadcast("Игра началась!")
+            self.broadcast("Игра началась!")
 
-                for client_socket in self.clients:
-                    threading.Thread(target=self.handle_client, args=(client_socket,)).start()
+            for client_socket in self.clients:
+                threading.Thread(target=self.handle_client, args=(client_socket,)).start()
 
         except Exception as e:
             print(f"Ошибка при запуске сервера: {e}")
@@ -52,7 +52,8 @@ class BattleServer:
         """Уведомление клиента о том, что сервер ожидает второго игрока."""
         try:
             while len(self.clients) < 2:
-                client_socket.send("Ожидаем второго игрока...".encode())
+                # client_socket.send(pickle.dumps("Ожидаем второго игрока..."))
+                self.broadcast("Ожидаем второго игрока...")
                 threading.Event().wait(5)  # Отправляем сообщение каждые 5 секунд
         except Exception as e:
             print(f"Ошибка при уведомлении клиента: {e}")
@@ -61,11 +62,27 @@ class BattleServer:
         """Обработка данных от клиента."""
         try:
             while True:
-
-                data = client_socket.recv(1024).decode()
-                if not data:
+                length_prefix = client_socket.recv(10)
+                if not length_prefix:
                     break
-                print(data)
+
+                # Преобразуем строку длины в целое число
+                data_len = int(length_prefix.decode('utf-8'))
+
+                # Читаем данные до тех пор, пока не будет считано нужное количество байтов
+                received_data = b""
+                while len(received_data) < data_len:
+                    chunk = client_socket.recv(data_len - len(received_data))
+                    if not chunk:
+                        break
+                    received_data += chunk
+
+                if len(received_data) != data_len:
+                    raise ValueError("Incomplete data received")
+
+                # Десериализуем данные
+                data = pickle.loads(received_data)
+
                 self.broadcast(data, client_socket)
 
         except Exception as e:
@@ -79,7 +96,14 @@ class BattleServer:
             for client in self.clients:
                 if client != sender_socket:
                     try:
-                        client.send(message.encode())
+                        # Данные в полном объеме
+                        serialized_data = pickle.dumps(message)
+                        # Создаем префиксы длины всех данных
+                        data_len = len(serialized_data)
+                        len_prefix = f"{data_len:010}".encode("utf-8")
+
+                        client.send(len_prefix + serialized_data)
+
                     except Exception as e:
                         print(f"Ошибка при отправке сообщения клиенту: {e}")
                         self.remove_client(client)
